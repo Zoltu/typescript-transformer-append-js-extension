b9ef448d6a81a00760a3658893bea86199b8b10c
Micah Zoltu <micah@zoltu.net>
2019-07-23 13:01:49 +0800

    Adds support for `.` in folder names and support for export statements.
    There will still be true negatives when a filename contains a dot that isn't used as an extension separator, but solving that requires actually resolving the module which is a hard problem and involves going to disk, which I would like to avoid.

--- tests/source/index.ts
+++ tests/source/index.ts
@@ -1,5 +1,10 @@
 import { foo } from './foo'
 import { bar } from './bar.js'
+import { baz } from './.baz/index'
+export { foo } from './foo'
+export { bar } from './bar.js'
+export { baz }
 
 foo()
 bar()
+baz()
