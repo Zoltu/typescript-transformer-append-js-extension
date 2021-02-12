fa8fc3288795d48b8051a70b3fb000a7336ccac2
Clinton Wood (MBP) <clint@anotherwaytech.com>
2021-02-12 09:59:45 +0000

    add node like resolution for directory module specifiers, update dependencies

--- source/index.ts
+++ source/index.ts
@@ -1,22 +1,14 @@
 import * as typescript from 'typescript'
 import * as path from 'path'
+import * as fs from 'fs'
 
-const transformer = (_: typescript.Program) => (transformationContext: typescript.TransformationContext) => (sourceFile: typescript.SourceFile) => {
-	function visitNode(node: typescript.Node): typescript.VisitResult<typescript.Node> {
-		if (shouldMutateModuleSpecifier(node)) {
-			if (typescript.isImportDeclaration(node)) {
-				const newModuleSpecifier = typescript.createLiteral(`${node.moduleSpecifier.text}.js`)
-				return typescript.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, newModuleSpecifier)
-			} else if (typescript.isExportDeclaration(node)) {
-				const newModuleSpecifier = typescript.createLiteral(`${node.moduleSpecifier.text}.js`)
-				return typescript.updateExportDeclaration(node, node.decorators, node.modifiers, node.exportClause, newModuleSpecifier)
-			}
-		}
+type ImportExportModuleSpecifier = (typescript.ImportDeclaration | typescript.ExportDeclaration) & { moduleSpecifier: typescript.StringLiteral }
 
-		return typescript.visitEachChild(node, visitNode, transformationContext)
-	}
+const transformer = (program: typescript.Program) => (transformationContext: typescript.TransformationContext) => (sourceFile: typescript.SourceFile) => {
+  const compilerOptions = program.getCompilerOptions()
+  const isNodeModuleResolution = compilerOptions.moduleResolution === typescript.ModuleResolutionKind.NodeJs
 
-	function shouldMutateModuleSpecifier(node: typescript.Node): node is (typescript.ImportDeclaration | typescript.ExportDeclaration) & { moduleSpecifier: typescript.StringLiteral } {
+  function shouldMutateModuleSpecifier(node: typescript.Node): node is ImportExportModuleSpecifier {
 		if (!typescript.isImportDeclaration(node) && !typescript.isExportDeclaration(node)) return false
 		if (node.moduleSpecifier === undefined) return false
 		// only when module specifier is valid
@@ -26,6 +18,43 @@ const transformer = (_: typescript.Program) => (transformationContext: typescrip
 		// only when module specifier has no extension
 		if (path.extname(node.moduleSpecifier.text) !== '') return false
 		return true
+	}
+
+  // use node like module resolution on the module specifier for cases where it points to a directory
+  // containing an index.ts which would ultimately results in module-specifier/index.js)
+  function resolveModuleSpecifier(node: ImportExportModuleSpecifier) {
+    const moduleSpecifier = node.moduleSpecifier.text
+    // only attempt node type resolution when module resolution is node
+    if (isNodeModuleResolution) {
+      // in case the node has prior transforms get the original node
+      const originalNode = typescript.getOriginalNode(node)
+      // try the current node first then revert to original node for source file
+      const sourceFile = node.getSourceFile() || originalNode.getSourceFile()
+      if (sourceFile) {
+        const absoluteModuleSpecifier = path.join(path.dirname(sourceFile.fileName), moduleSpecifier)
+        // if the module specifier is not pointing to a source file, use
+        // node like module resolution to try for <absoluteModuleSpecifier>/index.ts scheme
+        if (!fs.existsSync(`${absoluteModuleSpecifier}.ts`) && fs.existsSync(`${absoluteModuleSpecifier}${path.sep}index.ts`)) {
+          return `${moduleSpecifier}${path.sep}index.js`
+        }
+      }
+    }
+    // default to only append .js
+    return `${moduleSpecifier}.js`
+  }
+
+  function visitNode(node: typescript.Node): typescript.VisitResult<typescript.Node> {
+		if (shouldMutateModuleSpecifier(node)) {
+			if (typescript.isImportDeclaration(node)) {
+        const newModuleSpecifier = typescript.factory.createStringLiteral(resolveModuleSpecifier(node))
+        return typescript.factory.updateImportDeclaration(node, node.decorators, node.modifiers, node.importClause, newModuleSpecifier)
+			} else if (typescript.isExportDeclaration(node)) {
+        const newModuleSpecifier = typescript.factory.createStringLiteral(resolveModuleSpecifier(node))
+				return typescript.factory.updateExportDeclaration(node, node.decorators, node.modifiers, false, node.exportClause, newModuleSpecifier)
+			}
+		}
+
+		return typescript.visitEachChild(node, visitNode, transformationContext)
 	}
 
 	return typescript.visitNode(sourceFile, visitNode)
--- tests/source/index.ts
+++ tests/source/index.ts
@@ -1,6 +1,8 @@
 import { foo } from './foo'
 import { bar } from './bar.js'
-import { baz } from './.baz/index'
+// NOTE: When tsconfig "moduleResolution": "node" then './.baz' -> './.baz/index.js'
+//       otherwise you need to use './.baz/index'
+import { baz } from './.baz'
 export { foo } from "./foo"
 export { bar } from "./bar.js"
 export { baz }
